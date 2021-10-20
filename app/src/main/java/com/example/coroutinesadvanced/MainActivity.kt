f46d9bd12a55bcbb70e537a0c962970164dddb64
Ali <ali.cse233@gmail.com>
2021-10-20 17:06:13 +0300

    Added new activity
    Life cycle scope learning related to coroutines

--- app/src/main/java/com/example/coroutinesadvanced/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesadvanced/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.coroutinesadvanced
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 import kotlin.time.measureTime
@@ -60,6 +63,29 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+//        Till now we were using GlobalScope
+//        Which is not good practice as when we destroy our any component
+//        like when activity is finished if you used global scope then
+//        running coroutines will not be finished and Garbage collected
+//        for that you can use life cycle scopes
+
+
+        findViewById<TextView>(R.id.tvClick).setOnClickListener {
+            lifecycleScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(TAG, "still running")
+                }
+            }
+
+            GlobalScope.launch {
+                delay(3000L)
+                Intent(this@MainActivity, MainActivity2::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
     }
 
     private suspend fun networkCall1(): String {
